05d74064dfdc3da10997b69fba7cc417adf157f3
wulin <admin@MacBook.local>
2023-10-25 17:56:35 +0800

    fix: 解决箱子同时变色的问题

--- src/helpers/entity/box-graphic.ts
+++ src/helpers/entity/box-graphic.ts
@@ -5,13 +5,12 @@ import { BOX, colors } from '@/common/constants'
 
 import Graphic from './graphic'
 
-const BOX_GEOMETRY = new RoundedBoxGeometry(0.8, 0.8, 0.8, 5, 0.1)
-const BOX_MATERIAL = new MeshStandardMaterial({
-  color: colors.box
-})
-
 export default class BoxGraphic extends Graphic {
   constructor(color?: number | string) {
+    const BOX_GEOMETRY = new RoundedBoxGeometry(0.8, 0.8, 0.8, 5, 0.1)
+    const BOX_MATERIAL = new MeshStandardMaterial({
+      color: colors.box
+    })
     const mesh = new Mesh(BOX_GEOMETRY, BOX_MATERIAL)
     mesh.name = BOX
 
